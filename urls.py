d5890c0d16cc094055c9b5ef68e30584c0c9d055
ewheeler <evanmwheeler@gmail.com>
2011-02-17 18:08:30 -0500

    add styles, images to make it look like rapidsms

--- urls.py
+++ urls.py
@@ -1,9 +1,22 @@
+import os
 from django.conf.urls.defaults import *
+import settings
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    ('^$', 'django.views.generic.simple.direct_to_template',
-     {'template': 'home.html'}),
+
+    # RapidSMS core URLs
+    url(r'^$', 'views.dashboard', name='rapidsms-dashboard'),
+
+    # serve assets via django, during development
+    url(r'^rapidsms/javascripts/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/javascripts"}),
+    url(r'^rapidsms/stylesheets/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/stylesheets"}),
+    url(r'^rapidsms/images/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/images"}),
+    url(r'^rapidsms/icons/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/icons"}),
 )
