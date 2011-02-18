aa717cf1f1361e072a6bc79527d44a1c2609d945
ewheeler <evanmwheeler@gmail.com>
2011-02-17 19:53:52 -0500

    change urls so static stuff gets served by app.yaml

--- urls.py
+++ urls.py
@@ -12,14 +12,4 @@ urlpatterns = patterns('',
 
     # RapidSMS core URLs
     url(r'^$', 'views.dashboard', name='rapidsms-dashboard'),
-
-    # serve assets via django, during development
-    url(r'^rapidsms/javascripts/(?P<path>.*)$', "django.views.static.serve",
-    {"document_root": os.path.dirname(__file__) + "/static/javascripts"}),
-    url(r'^rapidsms/stylesheets/(?P<path>.*)$', "django.views.static.serve",
-    {"document_root": os.path.dirname(__file__) + "/static/stylesheets"}),
-    url(r'^rapidsms/images/(?P<path>.*)$', "django.views.static.serve",
-    {"document_root": os.path.dirname(__file__) + "/static/images"}),
-    url(r'^rapidsms/icons/(?P<path>.*)$', "django.views.static.serve",
-    {"document_root": os.path.dirname(__file__) + "/static/icons"}),
 )
